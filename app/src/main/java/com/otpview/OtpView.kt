ed333c50db7f759ad1c35db1f97424716b12a378
ankit <ankitchandel@zapbuild.com>
2019-03-11 10:11:07 +0530

    Otp view custom attribute

---
+++ app/src/main/java/com/otpview/OtpView.kt
@@ -0,0 +1,58 @@
+package com.otpview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.widget.EditText
+
+class OtpView  @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : EditText(context, attrs, defStyleAttr){
+
+    private var space:Float = 24f
+    private var charSize:Float = 0f
+    private var noOfChars:Float = 0f
+
+
+    init {
+        initView(context,attrs)
+    }
+
+    private fun initView(context: Context, attrs: AttributeSet?) {
+        setBackgroundResource(0)
+        val multi = context.resources.displayMetrics.density
+        space = multi * space // convert pixels in your density
+        val styleable =
+            context.theme.obtainStyledAttributes(attrs, R.styleable.OtpView, 0, 0)
+        noOfChars = styleable.getInteger(R.styleable.OtpView_otpLength,2).toFloat()
+
+    }
+
+
+    override fun onDraw(canvas: Canvas?) {
+        val availableWidth = width  - paddingRight - paddingLeft
+
+
+        if(space < 0){
+            charSize = (availableWidth / (noOfChars * 2 - 1))
+        }else{
+            charSize = (availableWidth - (space * (noOfChars - 1))) / noOfChars
+        }
+
+        var start = paddingLeft.toFloat()
+        val bottom = (height - paddingBottom).toFloat()
+
+
+        for (i in 0 ..noOfChars.toInt()){
+            canvas?.drawLine(start,bottom, start + charSize,bottom,paint)
+            if(space < 0){
+                start += charSize * 2
+            }else{
+                start += charSize + space
+            }
+        }
+
+    }
+
+
+}
